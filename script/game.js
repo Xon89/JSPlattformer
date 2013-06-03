001a93cb5e416bd729a626ec1c4e57ff11cdc1d2
Kevin <kevinseanb@gmail.com>
2013-06-03 15:10:55 +0200

    bilder an bewegung angepasst

--- script/game.js
+++ script/game.js
@@ -29,6 +29,7 @@ canvas.width = width;
 canvas.height = height;
 var mouse = utils.captureMouse(canvas);
 var planeposx = 800;
+drawTankmov = "stand";
 
 
 function update(){
@@ -76,16 +77,15 @@ function update(){
 	}
 	bgimg.src = "img/bg.jpg";
 
-	var tankimg = new Image();
-	tankimg.onload = function () {
-		ctx.drawImage(tankimg, player.x, player.y, player.width, player.height);
-	}
+	drawTank();
+	
 	var turretimg = new Image();
 	turretimg.onload = function() {
 			ctx.drawimage(turretimg, player.x-10, player.y, player.width, player.height);
 	}
-	tankimg.src = "img/tankbody.png";
-	turretimg.src ="img/Turret1.png"
+	
+	turretimg.src ="img/Turret1.png";
+	
 	enemyPlane();
 	
 	shoot();
@@ -102,17 +102,67 @@ function enemyPlane(){
 		planeimg.src = "img/plane.png";
 		
 		planeposx = planeposx - 5;
-	}
-	else
+		} else
 		planeposx = 800;
+}
+
+function drawTank() {
+	if( (drawTankmov == "stand") && !player.jumping) {
+		var tankstandimg = new Image();
+		tankstandimg.onload = function () {
+			ctx.drawImage(tankstandimg, player.x, player.y, player.width, player.height);
+		}
+		tankstandimg.src = "img/Tankbody.png";
+	} else if( (drawTankmov == "left") && !player.jumping) {
+		var tankleftimg = new Image();
+		tankleftimg.onload = function () {
+			ctx.drawImage(tankleftimg, player.x, player.y, player.width, player.height);
+		}
+		tankleftimg.src = "img/Tankleft.png";
+	} else if( (drawTankmov == "right") && !player.jumping) {
+		var tankrightimg = new Image();
+		tankrightimg.onload = function () {
+			ctx.drawImage(tankrightimg, player.x, player.y, player.width, player.height);
+		}
+		tankrightimg.src = "img/Tankright.png";
+	} else if( (drawTankmov == "stand") && player.jumping){
+		var tankupimg = new Image();
+		tankupimg.onload = function () {
+			ctx.drawImage(tankupimg, player.x, player.y, player.width, player.height);
+		}
+		tankupimg.src = "img/Tankup.png";
+	} else if( (drawTankmov == "left") && player.jumping){
+		var tankupleftimg = new Image();
+		tankupleftimg.onload = function () {
+			ctx.drawImage(tankupleftimg, player.x, player.y, player.width, player.height);
+		}
+		tankupleftimg.src = "img/Tankupleft.png";
+	} else if( (drawTankmov == "right") && player.jumping){
+		var tankuprightimg = new Image();
+		tankuprightimg.onload = function () {
+			ctx.drawImage(tankuprightimg, player.x, player.y, player.width, player.height);
+		}
+		tankuprightimg.src = "img/Tankupright.png";
+	}
 }
 
 document.body.addEventListener("keydown", function(e) {
     keys[e.keyCode] = true;
+    if(keys[37] || keys[65])
+    	drawTankmov = "left";
+    if(keys[39] || keys[68])
+    	drawTankmov = "right";
 });
 
 document.body.addEventListener("keyup", function(e) {
+    if(keys[37] || keys[65] || keys[39] || keys[68])
+    	tankinmovement = true;
     keys[e.keyCode] = false;
+    if((!keys[37] && !keys[65] ) && (!keys[39] && !keys[68]))
+    	tankinmovement = false;
+    if(!tankinmovement)
+    	drawTankmov = "stand";
+    tankinmovement = null;
 });
 
 canvas.addEventListener("mousedown", function(e) {
