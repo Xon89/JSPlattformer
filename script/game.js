dc46583d98db3daf16256a026e3d1a8c3a20e69e
twh <t.winkelsherding@googlemail.com>
2013-06-04 17:13:20 +0300

    letztes

--- script/game.js
+++ script/game.js
@@ -18,27 +18,11 @@ function update(){
  	requestAnimationFrame(update);
 }
 
-
-
-
-
 window.addEventListener("load",function(){
     update();
 });
 
 
 
-function drawBoxes(){
-//draw boxes and Fill
-	var img = new Image();
-	img.src="img/Tankbody.png";
-	var pat=ctx.createPattern(img,"repeat");
-	ctx.fillStyle=pat;
-	ctx.beginPath();
-	
-	for (var i = 0; i < boxes.length; i++) {
-    ctx.rect(boxes[i].x, boxes[i].y, boxes[i].width, boxes[i].height);
-	}
-	ctx.fill();
-}
+
 
