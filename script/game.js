86e2b3bef3f84e72f80b6a616337a95ec20ce9bd
twh <t.winkelsherding@googlemail.com>
2013-06-04 16:33:10 +0300

    cleanup

--- script/game.js
+++ script/game.js
@@ -1,25 +1,6 @@
 
 function update(){
-    if (keys[32]) {
-      //JUMP
-      if(!player.jumping){
-       player.jumping = true;
-       player.velY = -player.speed*2;
-      }
-    }
-    if (keys[39] || keys[68]) {
-        // MOVE RIGHT
-        if (player.velX < player.speed) {
-            player.velX++;
-        }
-    }
-    if (keys[37] || keys[65]) {
-        // MOVE LEFT
-        if (player.velX > -player.speed) {
-            player.velX--;
-        }
-    }
-
+	inputplayer();
     player.velX *= friction;
 
     player.velY += gravity;
